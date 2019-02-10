e21a182b04cb262d8b5544982c080ce86d40131f
Anna <apzhadovets@gmail.com>
2019-02-10 20:26:00 +0300

    создан вспомогательный класс с параметрами контакта (contact.py)

---
+++ contact.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+class Contact:
+
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company, address, home_phone, mobile_phone,
+                 work_phone, fax, primary_email, secondary_email, third_email, homepage, birth_day, birth_month,
+                 birth_year, anniversary_day, anniversary_month, anniversary_year, secondary_address,
+                 secondary_home_phone, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.primary_email = primary_email
+        self.secondary_email = secondary_email
+        self.third_email = third_email
+        self.homepage = homepage
+        self.birth_day = birth_day
+        self.birth_month = birth_month
+        self.birth_year = birth_year
+        self.anniversary_day = anniversary_day
+        self.anniversary_month = anniversary_month
+        self.anniversary_year = anniversary_year
+        self.secondary_address = secondary_address
+        self.secondary_home_phone = secondary_home_phone
+        self.notes = notes
