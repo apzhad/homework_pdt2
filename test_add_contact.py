df0656a5ef2a64658da14a273320eb88d19fd5be
Anna <apzhadovets@gmail.com>
2019-02-16 13:10:05 +0300

    добавлена функция инициализации фикстуры; класс заменен на функцию (в кчестве параметра передается фикстура); используется тестовый фреймворк pytest

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,30 +1,25 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from contact import Contact
-
 from generic import Generic
 
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.gen = Generic()
-
-    def test_add_contact(self):
-        self.gen.login(username="admin", password="secret")
-        self.gen.create_contact(Contact(first_name="first_name", middle_name="middle_name", last_name="last_name",
-                                    nickname="nickname", title="title", company="company", address="address",
-                                    home_phone="homephone", mobile_phone="mobilephone", work_phone="workphone",
-                                    fax="fax", primary_email="email", secondary_email="email2",
-                                    third_email="email3", homepage="homepage", birth_day="5", birth_month="January",
-                                    birth_year="1950", anniversary_day="15", anniversary_month="June",
-                                    anniversary_year="2000", group_name="[none]",
-                                    secondary_address="address secondary", secondary_home_phone="home secondary",
-                                    notes="notes", photo_path="\\test_data\\cat.jpg"))
-        self.gen.logout()
-
-    def tearDown(self):
-        self.gen.finish()
+@pytest.fixture
+def gen(request):
+    fixture = Generic()
+    request.addfinalizer(fixture.finish)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(gen):
+    gen.login(username="admin", password="secret")
+    gen.create_contact(Contact(first_name="first_name", middle_name="middle_name", last_name="last_name",
+                               nickname="nickname", title="title", company="company", address="address",
+                               home_phone="homephone", mobile_phone="mobilephone", work_phone="workphone",
+                               fax="fax", primary_email="email", secondary_email="email2",
+                               third_email="email3", homepage="homepage", birth_day="5", birth_month="January",
+                               birth_year="1950", anniversary_day="15", anniversary_month="June",
+                               anniversary_year="2000", group_name="[none]",
+                               secondary_address="address secondary", secondary_home_phone="home secondary",
+                               notes="notes", photo_path="\\test_data\\cat.jpg"))
+    gen.logout()
