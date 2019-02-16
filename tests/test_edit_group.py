0bf98326efedf3f830b832c6fb862ff16fa7ed19
Anna <apzhadovets@gmail.com>
2019-02-17 00:11:14 +0300

    в test_edit_group добавлены тесты: * очистка параметров группы; * обновление параметров группы без изменений

--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -5,3 +5,15 @@ def test_edit_first_group(gen):
     gen.session.login(username="admin", password="secret")
     gen.group.edit_first(Group(name="new_name", header="new_header_group", footer="changed_group"))
     gen.session.logout()
+
+
+def test_clear_first_group_params(gen):
+    gen.session.login(username="admin", password="secret")
+    gen.group.edit_first(Group(name="", header="", footer=""))
+    gen.session.logout()
+
+
+def test_update_first_group_without_changes(gen):
+    gen.session.login(username="admin", password="secret")
+    gen.group.update_first_wo_change()
+    gen.session.logout()
