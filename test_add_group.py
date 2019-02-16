84e119c49af7bb6a289a64f0033e4c255c51090e
Anna <apzhadovets@gmail.com>
2019-02-16 12:42:27 +0300

    используется тестовый фреймворк pytest; добавлена функция инициализации фикстуры; тесты заменены на обычные функции; в тесты фикстура передаётся в качестве параметра

--- test_add_group.py
+++ test_add_group.py
@@ -1,38 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from group import Group
 from generic import Generic
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.gen = Generic()
-
-    def test_add_group(self):
-        self.gen.login(username="admin", password="secret")
-        self.gen.create_new_group(Group(name="new_group", header="header_group", footer="first_group"))
-        self.gen.logout()
-
-    def test_add_empty_group(self):
-        self.gen.login(username="admin", password="secret")
-        self.gen.create_new_group(Group(name="", header="", footer=""))
-        self.gen.logout()
-
-    def tearDown(self):
-        self.gen.finish()
-
-    def is_element_present(self, how, what):
-        try: self.gen.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.gen.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
+@pytest.fixture
+def gen(request):
+    fixture = Generic()
+    request.addfinalizer(fixture.finish)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_group(gen):
+    gen.login(username="admin", password="secret")
+    gen.create_new_group(Group(name="new_group", header="header_group", footer="first_group"))
+    gen.logout()
+
+
+def test_add_empty_group(gen):
+    gen.login(username="admin", password="secret")
+    gen.create_new_group(Group(name="", header="", footer=""))
+    gen.logout()
