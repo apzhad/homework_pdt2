bac3a153a37c033ba99adc17b8f1361ab1b11ee8
Anna <apzhadovets@gmail.com>
2019-02-16 13:04:53 +0300

    из создания контакта убраны вспомогательные методы; используется класс фикстуры generic

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
 from contact import Contact
-import os
+
+from generic import Generic
 
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.gen = Generic()
 
     def test_add_contact(self):
-        self.login(username="admin", password="secret")
-        self.create_contact(Contact(first_name="first_name", middle_name="middle_name", last_name="last_name",
+        self.gen.login(username="admin", password="secret")
+        self.gen.create_contact(Contact(first_name="first_name", middle_name="middle_name", last_name="last_name",
                                     nickname="nickname", title="title", company="company", address="address",
                                     home_phone="homephone", mobile_phone="mobilephone", work_phone="workphone",
                                     fax="fax", primary_email="email", secondary_email="email2",
@@ -24,146 +20,10 @@ class TestAddContact(unittest.TestCase):
                                     anniversary_year="2000", group_name="[none]",
                                     secondary_address="address secondary", secondary_home_phone="home secondary",
                                     notes="notes", photo_path="\\test_data\\cat.jpg"))
-        self.logout()
+        self.gen.logout()
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_homepage(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home page").click()
-
-    def create_contact(self, contact):
-        wd = self.wd
-        # init new contact creation
-        wd.find_element_by_link_text("add new").click()
-        # enter contact parameters
-        # contact name
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-
-        # add photo
-        photo = wd.find_element_by_name("photo")
-        photo.send_keys(os.getcwd() + contact.photo_path)
-        """
-        # code from recoder
-        wd.find_element_by_name("photo").click()
-        wd.find_element_by_name("photo").clear()
-        wd.find_element_by_name("photo").send_keys(os.getcwd() + "\\test_data\\cat.jpg")
-        """
-
-        # common info (company, address,title, etc.)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        # phone info
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home_phone)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work_phone)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        # email info
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.primary_email)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.secondary_email)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.third_email)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        # birthday info
-        wd.find_element_by_name("bday").click()
-        # enter value from keyboard
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.birth_day)
-        # select value by mouse click
-        wd.find_element_by_xpath("//option[@value='%s']" % contact.birth_day).click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.birth_month)
-        wd.find_element_by_xpath("//option[@value='%s']" % contact.birth_month).click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.birth_year)
-        # anniversary info
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.anniversary_day)
-        wd.find_element_by_xpath("(//option[@value='%s'])[2]" % contact.anniversary_day).click()
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.anniversary_month)
-        wd.find_element_by_xpath("(//option[@value='%s'])[2]" % contact.anniversary_month).click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contact.anniversary_year)
-        # select group
-        wd.find_element_by_name("new_group").click()
-        wd.find_element_by_xpath("//option[@value='%s']" % contact.group_name).click()
-        # secondary info
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.secondary_address)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.secondary_home_phone)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
-        # submit contact creation
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        self.return_to_homepage()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
     def tearDown(self):
-        self.wd.quit()
+        self.gen.finish()
 
 
 if __name__ == "__main__":
