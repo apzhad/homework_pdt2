6d226f69094cf83d0ab2212a99c45dfda9744e23
Anna <apzhadovets@gmail.com>
2019-02-17 17:08:31 +0300

    в fixture.group добавлены методы удаления групп: * удалить все группы del_all; * удалить последнюю del_last; * нажать кнопку удалить без выбора группы del_not_choose

--- fixture/group.py
+++ fixture/group.py
@@ -68,3 +68,30 @@ class GroupManage:
         wd.find_element_by_name("edit").click()
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+
+    def del_all(self):
+        wd = self.gen.wd
+        self.open_groups_page()
+        # get group count
+        group_count = len(wd.find_elements_by_name("selected[]"))
+        # select all groups & submit deletion
+        for i in range(group_count):
+            wd.find_element_by_xpath("(//input[@name='selected[]'])[%s]" % (i+1)).click()
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def del_last(self):
+        wd = self.gen.wd
+        self.open_groups_page()
+        # get group count
+        group_count = len(wd.find_elements_by_name("selected[]"))
+        # select last groups & submit deletion
+        wd.find_element_by_xpath("(//input[@name='selected[]'])[%s]" % group_count).click()
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def del_not_choose(self):
+        wd = self.gen.wd
+        self.open_groups_page()
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
