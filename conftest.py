81cec87d865db292ea9e6479e3897a2493ef577d
Anna <apzhadovets@gmail.com>
2019-02-23 19:30:29 +0300

    валидация фикстуры перед каждым тестом

--- conftest.py
+++ conftest.py
@@ -1,12 +1,23 @@
 import pytest
 from fixture.generic import Generic
 
+fixture = None
 
-@pytest.fixture(scope="session")
-def gen(request):
-    fixture = Generic()
-    fixture.session.login(username="admin", password="secret")
 
+@pytest.fixture
+def gen():
+    global fixture
+    if fixture is None:
+        fixture = Generic()
+        fixture.session.login(username="admin", password="secret")
+    elif not fixture.is_valid():
+        fixture = Generic()
+        fixture.session.login(username="admin", password="secret")
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
     def fin():
         fixture.session.logout()
         fixture.finish()
