a6495094e6aa1fb26db3f0bc33db5bc726ba4f8d
Anna <apzhadovets@gmail.com>
2019-02-23 19:29:57 +0300

    добавлен метод перехода на начальную страницу и его использование

--- fixture/contact.py
+++ fixture/contact.py
@@ -8,57 +8,9 @@ class ContactManage:
     def __init__(self, gen):
         self.gen = gen
 
-    def create(self, contact):
+    def open_home_page(self):
         wd = self.gen.wd
-        # init new contact creation
-        wd.find_element_by_link_text("add new").click()
-        # enter contact parameters
-        self.enter_contact_parameters(contact)
-        # submit contact creation
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        self.return_to_homepage()
-
-    def return_to_homepage(self):
-        wd = self.gen.wd
-        wd.find_element_by_link_text("home page").click()
-
-    def create_empty(self):
-        wd = self.gen.wd
-        # init new contact creation
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        self.return_to_homepage()
-
-    def del_first(self):
-        wd = self.gen.wd
-        # select first contact & submit deletion
-        self.select_first_contact()
-        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
-        wd.switch_to_alert().accept()
-
-    def select_first_contact(self):
-        wd = self.gen.wd
-        wd.find_element_by_name("selected[]").click()
-
-    def cancel_del_first(self):
-        wd = self.gen.wd
-        # select contact & submit deletion
-        self.select_first_contact()
-        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
-        wd.switch_to_alert().dismiss()
-
-    def del_by_select_all(self):
-        wd = self.gen.wd
-        # click "select all" & submit deletion
-        wd.find_element_by_xpath("(//input[@id='MassCB'])").click()
-        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
-        wd.switch_to_alert().accept()
-
-    def del_all_by_click(self):
-        wd = self.gen.wd
-        self.select_all_contact()
-        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
-        wd.switch_to_alert().accept()
+        wd.find_element_by_link_text("home").click()
 
     def select_all_contact(self):
         wd = self.gen.wd
@@ -66,34 +18,6 @@ class ContactManage:
         contact_count = len(wd.find_elements_by_name("selected[]"))
         for i in range(contact_count):
             wd.find_element_by_xpath("(//input[@name='selected[]'])[%s]" % (i + 1)).click()
-
-    def del_unselected(self):
-        wd = self.gen.wd
-        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
-        wd.switch_to_alert().accept()
-
-    def del_all_from_group(self, group_name):
-        wd = self.gen.wd
-        Select(wd.find_element_by_name("group")).select_by_visible_text(group_name)
-        # click "select all" & submit deletion
-        wd.find_element_by_xpath("(//input[@id='MassCB'])").click()
-        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
-        wd.switch_to_alert().accept()
-
-    def del_first_from_group(self, group_name):
-        wd = self.gen.wd
-        Select(wd.find_element_by_name("group")).select_by_visible_text(group_name)
-        # click "select all" & submit deletion
-        self.select_first_contact()
-        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
-        wd.switch_to_alert().accept()
-
-    def edit_first(self, contact):
-        wd = self.gen.wd
-        wd.find_element_by_xpath("//img[@alt='Edit']").click()
-        self.enter_contact_parameters(contact)
-        wd.find_element_by_name("update").click()
-        self.return_to_homepage()
 
     def enter_contact_parameters(self, contact):
         wd = self.gen.wd
@@ -165,19 +89,110 @@ class ContactManage:
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
 
+    def create(self, contact):
+        wd = self.gen.wd
+        # init new contact creation
+        wd.find_element_by_link_text("add new").click()
+        # enter contact parameters
+        self.enter_contact_parameters(contact)
+        # submit contact creation
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        self.return_to_homepage()
+
+    def return_to_homepage(self):
+        wd = self.gen.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def select_first_contact(self):
+        wd = self.gen.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def create_empty(self):
+        wd = self.gen.wd
+        # init new contact creation
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        self.return_to_homepage()
+
+    def del_first(self):
+        wd = self.gen.wd
+        self.open_home_page()
+        # select first contact & submit deletion
+        self.select_first_contact()
+        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
+        wd.switch_to_alert().accept()
+
+    def cancel_del_first(self):
+        wd = self.gen.wd
+        self.open_home_page()
+        # select contact & submit deletion
+        self.select_first_contact()
+        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
+        wd.switch_to_alert().dismiss()
+
+    def del_by_select_all(self):
+        wd = self.gen.wd
+        self.open_home_page()
+        # click "select all" & submit deletion
+        wd.find_element_by_xpath("(//input[@id='MassCB'])").click()
+        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
+        wd.switch_to_alert().accept()
+
+    def del_all_by_click(self):
+        wd = self.gen.wd
+        self.open_home_page()
+        self.select_all_contact()
+        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
+        wd.switch_to_alert().accept()
+
+    def del_unselected(self):
+        wd = self.gen.wd
+        self.open_home_page()
+        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
+        wd.switch_to_alert().accept()
+
+    def del_all_from_group(self, group_name):
+        wd = self.gen.wd
+        self.open_home_page()
+        self.select_from_list("group", group_name)
+        # click "select all" & submit deletion
+        wd.find_element_by_xpath("(//input[@id='MassCB'])").click()
+        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
+        wd.switch_to_alert().accept()
+
+    def del_first_from_group(self, group_name):
+        wd = self.gen.wd
+        self.open_home_page()
+        self.select_from_list("group", group_name)
+        # click "select all" & submit deletion
+        self.select_first_contact()
+        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
+        wd.switch_to_alert().accept()
+
+    def edit_first(self, contact):
+        wd = self.gen.wd
+        self.open_home_page()
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        self.enter_contact_parameters(contact)
+        wd.find_element_by_name("update").click()
+        self.return_to_homepage()
+
     def del_first_using_edit(self):
         wd = self.gen.wd
+        self.open_home_page()
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
 
     def edit_first_wo_change(self):
         wd = self.gen.wd
+        self.open_home_page()
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         wd.find_element_by_name("update").click()
         self.return_to_homepage()
 
     def edit_first_from_details(self, contact):
         wd = self.gen.wd
+        self.open_home_page()
         wd.find_element_by_xpath("//img[@alt='Details']").click()
         wd.find_element_by_name("modifiy").click()
         self.enter_contact_parameters(contact)
@@ -186,6 +201,7 @@ class ContactManage:
 
     def edit_first_in_group(self, group_name, contact):
         wd = self.gen.wd
+        self.open_home_page()
         self.select_from_list("group", group_name)
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         self.enter_contact_parameters(contact)
@@ -194,6 +210,7 @@ class ContactManage:
 
     def add_to_group(self, group_name):
         wd = self.gen.wd
+        self.open_home_page()
         wd.find_element_by_xpath("(//input[@id='MassCB'])").click()
         self.select_from_list("to_group", group_name)
         wd.find_element_by_name("add").click()
@@ -201,11 +218,13 @@ class ContactManage:
 
     def add_to_group_unselected(self, group_name):
         wd = self.gen.wd
+        self.open_home_page()
         self.select_from_list("to_group", group_name)
         wd.find_element_by_name("add").click()
 
     def add_to_group_from_another(self, group_from, group_to):
         wd = self.gen.wd
+        self.open_home_page()
         self.select_from_list("group", group_from)
         wd.find_element_by_xpath("(//input[@id='MassCB'])").click()
         self.select_from_list("to_group", group_to)
@@ -214,14 +233,16 @@ class ContactManage:
 
     def remove_from_group(self, group_name):
         wd = self.gen.wd
+        self.open_home_page()
         self.select_from_list("group", group_name)
         wd.find_element_by_xpath("(//input[@id='MassCB'])").click()
         wd.find_element_by_name("remove").click()
         wd.find_element_by_link_text("group page \"%s\"" % group_name).click()
-        self.select_from_list("group", "[all]")
 
     def del_all_found(self, search):
         wd = self.gen.wd
+        self.open_home_page()
+        self.select_from_list("group", "[all]")
         # find text
         self.set_field_value("searchstring", search)
         # click "select all" & submit deletion
