bb4ca6df73088730469e8c86a544e401c2692546
Anna <apzhadovets@gmail.com>
2019-02-24 20:43:33 +0300

    в return_to_groups_page добавлена проверка открыта ли страница групп

--- fixture/group.py
+++ fixture/group.py
@@ -13,7 +13,8 @@ class GroupManage:
 
     def return_to_groups_page(self):
         wd = self.gen.wd
-        wd.find_element_by_link_text("group page").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("group page").click()
 
     def create(self, group):
         wd = self.gen.wd
