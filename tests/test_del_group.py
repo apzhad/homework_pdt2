45eae9411810c5ee54957fd6e413dea2c1cc2277
Anna <apzhadovets@gmail.com>
2019-03-02 13:15:17 +0300

    реализованы проверки для удаления групп

--- tests/test_del_group.py
+++ tests/test_del_group.py
@@ -4,7 +4,12 @@ from model.group import Group
 def test_del_first_group(gen):
     if gen.group.get_group_count() == 0:
         gen.group.create(Group(name="for_del"))
+    old_group_list = gen.group.get_group_list()
     gen.group.del_first()
+    new_group_list = gen.group.get_group_list()
+    assert len(old_group_list) - 1 == len(new_group_list)
+    old_group_list[0:1] = []
+    assert old_group_list == new_group_list
 
 
 def test_del_all_group(gen):
@@ -13,6 +18,9 @@ def test_del_all_group(gen):
         gen.group.create(Group(name="for_del_2"))
         gen.group.create(Group(name="for_del_3"))
     gen.group.del_all()
+    new_group_list = gen.group.get_group_list()
+    assert 0 == len(new_group_list)
+    assert [] == new_group_list
 
 
 def test_del_last_group(gen):
@@ -20,11 +28,20 @@ def test_del_last_group(gen):
         gen.group.create(Group(name="for_del_1"))
         gen.group.create(Group(name="for_del_2"))
         gen.group.create(Group(name="for_del_3"))
+    old_group_list = gen.group.get_group_list()
     gen.group.del_last()
+    new_group_list = gen.group.get_group_list()
+    assert len(old_group_list) - 1 == len(new_group_list)
+    old_group_list[-1:] = []
+    assert old_group_list == new_group_list
 
 
 def test_del_without_choice_group(gen):
     if gen.group.get_group_count() == 0:
         gen.group.create(Group(name="for_del_1"))
         gen.group.create(Group(name="for_del_2"))
+    old_group_list = gen.group.get_group_list()
     gen.group.del_not_choose()
+    new_group_list = gen.group.get_group_list()
+    assert len(old_group_list) == len(new_group_list)
+    assert old_group_list == new_group_list
