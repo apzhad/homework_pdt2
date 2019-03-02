e2376a13f54cf79c9527d6561bf86bcf2092a02a
Anna <apzhadovets@gmail.com>
2019-03-02 18:21:00 +0300

    реализованы проверки для редактирования групп

--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -4,19 +4,37 @@ from model.group import Group
 def test_edit_first_group(gen):
     if gen.group.get_group_count() == 0:
         gen.group.create(Group(name="for_edit"))
-    gen.group.edit_first(Group(name="new_name", header="new_header_group", footer="changed_group"))
+    old_group_list = gen.group.get_group_list()
+    group = Group(name="new_name", header="new_header_group", footer="changed_group")
+    group.id = old_group_list[0].id
+    gen.group.edit_first(group)
+    new_group_list = gen.group.get_group_list()
+    assert len(old_group_list) == len(new_group_list)
+    old_group_list[0] = group
+    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
 
 
 def test_clear_first_group_params(gen):
     if gen.group.get_group_count() == 0:
         gen.group.create(Group(name="for_edit", header="header", footer="footer"))
-    gen.group.edit_first(Group(name="", header="", footer=""))
+    old_group_list = gen.group.get_group_list()
+    group = Group(name="", header="", footer="")
+    group.id = old_group_list[0].id
+    gen.group.edit_first(group)
+    new_group_list = gen.group.get_group_list()
+    assert len(old_group_list) == len(new_group_list)
+    old_group_list[0] = group
+    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
 
 
 def test_update_first_group_without_changes(gen):
     if gen.group.get_group_count() == 0:
         gen.group.create(Group(name="for_edit", header="header", footer="footer"))
+    old_group_list = gen.group.get_group_list()
     gen.group.update_first_wo_change()
+    new_group_list = gen.group.get_group_list()
+    assert len(old_group_list) == len(new_group_list)
+    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
 
 
 def test_update_all_groups(gen):
@@ -24,7 +42,14 @@ def test_update_all_groups(gen):
         gen.group.create(Group(name="for_edit", header="header", footer="footer"))
         gen.group.create(Group(name="for_edit_1", header="header_1", footer="footer_1"))
         gen.group.create(Group(name="for_edit_2", header="header_2", footer="footer_2"))
-    gen.group.edit_all(Group(name="some_name", header="some_header_group", footer="changed_group"))
+    old_group_list = gen.group.get_group_list()
+    group = Group(name="some_name", header="some_header_group", footer="changed_group")
+    group.id = old_group_list[0].id
+    gen.group.edit_all(group)
+    new_group_list = gen.group.get_group_list()
+    assert len(old_group_list) == len(new_group_list)
+    old_group_list[0] = group
+    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
 
 
 def test_update_all_groups_without_changes(gen):
@@ -32,24 +57,46 @@ def test_update_all_groups_without_changes(gen):
         gen.group.create(Group(name="for_edit", header="header", footer="footer"))
         gen.group.create(Group(name="for_edit_1", header="header_1", footer="footer_1"))
         gen.group.create(Group(name="for_edit_2", header="header_2", footer="footer_2"))
+    old_group_list = gen.group.get_group_list()
     gen.group.edit_all_wo_change()
+    new_group_list = gen.group.get_group_list()
+    assert len(old_group_list) == len(new_group_list)
+    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
 
 
 def test_update_last_group(gen):
     if gen.group.get_group_count() == 0:
         gen.group.create(Group(name="for_edit", header="header", footer="footer"))
         gen.group.create(Group(name="for_edit_1", header="header_1", footer="footer_1"))
-    gen.group.edit_last(Group(name="last_name", header="last_header_group", footer="last_group"))
+    old_group_list = gen.group.get_group_list()
+    group = Group(name="last_name", header="last_header_group", footer="last_group")
+    group.id = old_group_list[-1].id
+    gen.group.edit_last(group)
+    new_group_list = gen.group.get_group_list()
+    assert len(old_group_list) == len(new_group_list)
+    old_group_list[-1] = group
+    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
 
 
 def test_update_last_group_without_changes(gen):
     if gen.group.get_group_count() == 0:
         gen.group.create(Group(name="for_edit", header="header", footer="footer"))
         gen.group.create(Group(name="for_edit_1", header="header_1", footer="footer_1"))
+    old_group_list = gen.group.get_group_list()
     gen.group.update_last_wo_change()
+    new_group_list = gen.group.get_group_list()
+    assert len(old_group_list) == len(new_group_list)
+    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
 
 
 def test_edit_first_group_name(gen):
     if gen.group.get_group_count() == 0:
         gen.group.create(Group(name="for_edit", header="header", footer="footer"))
-    gen.group.edit_first(Group(name="only_name"))
+    old_group_list = gen.group.get_group_list()
+    group = Group(name="only_name")
+    group.id = old_group_list[0].id
+    gen.group.edit_first(group)
+    new_group_list = gen.group.get_group_list()
+    assert len(old_group_list) == len(new_group_list)
+    old_group_list[0] = group
+    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
