d66739bb4c47b7c4cf56350f9f0e42a7e814c472
Anna <apzhadovets@gmail.com>
2019-03-02 13:58:11 +0300

    реализованы проверки для создания групп

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -4,16 +4,22 @@ from model.group import Group
 
 def test_add_group(gen):
     old_group_list = gen.group.get_group_list()
-    gen.group.create(Group(name="new_group", header="header_group", footer="first_group"))
+    group = Group(name="new_group", header="header_group", footer="first_group")
+    gen.group.create(group)
     new_group_list = gen.group.get_group_list()
     assert len(old_group_list) + 1 == len(new_group_list)
+    old_group_list.append(group)
+    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
 
 
 def test_add_empty_group(gen):
     old_group_list = gen.group.get_group_list()
-    gen.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    gen.group.create(group)
     new_group_list = gen.group.get_group_list()
     assert len(old_group_list) + 1 == len(new_group_list)
+    old_group_list.append(group)
+    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
 
 
 def test_add_empty_v2(gen):
@@ -21,3 +27,5 @@ def test_add_empty_v2(gen):
     gen.group.create_empty()
     new_group_list = gen.group.get_group_list()
     assert len(old_group_list) + 1 == len(new_group_list)
+    old_group_list.append(Group(name=""))
+    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
