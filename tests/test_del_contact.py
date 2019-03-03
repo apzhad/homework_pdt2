9c7c0f82dd65c9329d33d8e3a268fd930a8d34ce
Anna <apzhadovets@gmail.com>
2019-03-03 21:20:09 +0300

    добавлены тесты удаления случайной группы

--- tests/test_del_contact.py
+++ tests/test_del_contact.py
@@ -1,5 +1,6 @@
 from model.contact import Contact
 from model.group import Group
+from random import randrange
 
 
 def test_del_first_contact(gen):
@@ -13,11 +14,34 @@ def test_del_first_contact(gen):
     assert old_contact_list == new_contact_list
 
 
+def test_del_some_contact(gen):
+    if gen.contact.get_contact_count() == 0:
+        gen.contact.create(Contact(first_name="del_contact", fax="573-092", nickname="1"))
+    old_contact_list = gen.contact.get_contact_list()
+    index = randrange(len(old_contact_list))
+    gen.contact.del_by_index(index)
+    assert len(old_contact_list) - 1 == gen.contact.get_contact_count()
+    new_contact_list = gen.contact.get_contact_list()
+    old_contact_list[index:index+1] = []
+    assert old_contact_list == new_contact_list
+
+
 def test_cancel_del_first_contact(gen):
     if gen.contact.get_contact_count() == 0:
         gen.contact.create(Contact(first_name="del_contact", fax="573-092", nickname="1"))
     old_contact_list = gen.contact.get_contact_list()
     gen.contact.cancel_del_first()
+    assert len(old_contact_list) == gen.contact.get_contact_count()
+    new_contact_list = gen.contact.get_contact_list()
+    assert old_contact_list == new_contact_list
+
+
+def test_cancel_del_some_contact(gen):
+    if gen.contact.get_contact_count() == 0:
+        gen.contact.create(Contact(first_name="del_contact", fax="573-092", nickname="1"))
+    old_contact_list = gen.contact.get_contact_list()
+    index = randrange(len(old_contact_list))
+    gen.contact.cancel_del_by_index(index)
     assert len(old_contact_list) == gen.contact.get_contact_count()
     new_contact_list = gen.contact.get_contact_list()
     assert old_contact_list == new_contact_list
@@ -80,6 +104,21 @@ def test_del_first_contact_from_group(gen):
     assert len(old_contact_list) - 1 == gen.contact.get_contact_count(group_name=group)
     new_contact_list = gen.contact.get_contact_list(group_name=group)
     old_contact_list[0:1] = []
+    assert old_contact_list == new_contact_list
+
+
+def test_del_some_contact_from_group(gen):
+    group = "group"
+    if group not in str(gen.group.get_group_list()) and group != "[all]" and group != "[none]":
+        gen.group.create(group=Group(name=group))
+    if gen.contact.get_contact_count(group_name=group) == 0:
+        gen.contact.create(Contact(first_name="del_contact", fax="573-092", nickname="1", group_name=group))
+    old_contact_list = gen.contact.get_contact_list(group_name=group)
+    index = randrange(len(old_contact_list))
+    gen.contact.del_from_group_by_index(index=index, group_name=group)
+    assert len(old_contact_list) - 1 == gen.contact.get_contact_count(group_name=group)
+    new_contact_list = gen.contact.get_contact_list(group_name=group)
+    old_contact_list[index:index+1] = []
     assert old_contact_list == new_contact_list
 
 
