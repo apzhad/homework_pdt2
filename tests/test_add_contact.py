3e7624e9fdedc798f319aa74d46c2d25de9f331b
Anna <apzhadovets@gmail.com>
2019-03-10 00:28:50 +0300

    сделана нормализация имени группы для провеки: - теперь используются случайно сгенерированные строки - для нормализации проверки удаляются двойные пробелы в строке - для нормализации проверки удаляются пробелы из конца

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -26,17 +26,19 @@ test_data = [Contact(first_name="", last_name="")] + [
             last_name=random_string("last_name", 10), nickname=random_string("nickname", 10),
             title=random_string("title", 10), company=random_string("company", 10),
             address=random_string_with_eol("address", 20), home_phone=random_string("homephone", 15),
-            mobile_phone=random_string("mobilephone", 15), work_phone=random_string("workphone", 15),
-            fax=random_string("fax", 15), primary_email=random_string("email", 10),
-            secondary_email=random_string("email2", 10), third_email=random_string("email3", 10),
-            homepage=random_string("homepage", 10))
+            fax=random_string("fax", 15), primary_email=random_string("email", 10))
     for i in range(3)
 ] + [
     Contact(first_name=random_string("first_name", 20), last_name=random_string("last_name", 10),
-            nickname=random_string("nickname", 10), address=random_string_with_eol("address", 20),
-            home_phone=random_string("homephone", 15), fax=random_string("fax", 15),
-            primary_email=random_string("email", 10), birth_day=str(random.randint(1, 31)),
-            birth_month=random.choice(month), birth_year=random_string("", 10),
+            title=random_string("title", 10), company=random_string("company", 10),
+            mobile_phone=random_string("mobilephone", 15), work_phone=random_string("workphone", 15),
+            secondary_email=random_string("email2", 10), third_email=random_string("email3", 10),
+            homepage=random_string("homepage", 10))
+    for m in range(3)
+] + [
+    Contact(first_name=random_string("first_name", 20), last_name=random_string("last_name", 10),
+            address=random_string_with_eol("address", 20), home_phone=random_string("homephone", 15),
+            birth_day=str(random.randint(1, 31)), birth_month=random.choice(month), birth_year=random_string("", 10),
             anniversary_day=str(random.randint(1, 31)), anniversary_month=random.choice(month),
             anniversary_year=random_string("", 10), secondary_address=random_string_with_eol("sec_address", 20))
     for j in range(3)
@@ -53,7 +55,7 @@ test_data = [Contact(first_name="", last_name="")] + [
 def delete_whitespace(s):
     if len(s) > 0:
         s = re.sub('\s+', ' ', s)
-        while s[-1] == " ":
+        if s[-1] == " ":
             s = s[:-1]
     return s
 
@@ -66,8 +68,6 @@ def normalize_contact_for_check(contact):
 
 @pytest.mark.parametrize("contact", test_data, ids=[repr(x) for x in test_data])
 def test_add_contact(gen, contact):
-    group = gen.group.get_groups_names() + ["[none]"]
-    contact.group_name = random.choice(group)
     old_contact_list = gen.contact.get_contact_list()
     gen.contact.create(contact)
     assert len(old_contact_list) + 1 == gen.contact.get_contact_count()
@@ -77,17 +77,17 @@ def test_add_contact(gen, contact):
 
 
 def test_add_contact_into_group(gen):
-    group = "new_name"
-    if group not in gen.group.get_groups_names() and group != "[all]" and group != "[none]":
-        gen.group.create(group=Group(name=group))
+    group = gen.group.get_groups_names() + ["[none]"]
     old_contact_list = gen.contact.get_contact_list()
     cont = Contact(first_name="first_name ", middle_name="middle_name", last_name="last_name", nickname="nickname",
                    title="title", company="company", address="address", home_phone="homephone",
                    mobile_phone="mobilephone", work_phone="workphone", fax="fax", primary_email="email",
                    secondary_email="email2", third_email="email3", homepage="homepage", birth_day="1",
                    birth_month=random.choice(month), birth_year="1950", anniversary_day="15", anniversary_month="June",
-                   anniversary_year="2000", group_name=group, secondary_address="address secondary",
+                   anniversary_year="2000", secondary_address="address secondary",
                    secondary_home_phone="home secondary", notes="notes", photo_path="\\tests\\test_data\\cat.jpg")
+
+    cont.group_name = random.choice(group)
     gen.contact.create(cont)
     assert len(old_contact_list) + 1 == gen.contact.get_contact_count()
     new_contact_list = gen.contact.get_contact_list()
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -3,6 +3,7 @@ from model.group import Group
 import pytest
 import random
 import string
+import re
 
 
 def random_string(prefix, max_length):
@@ -16,23 +17,37 @@ test_data = [Group(name="", header="", footer="")] + [
 ]
 
 
+def delete_whitespace(s):
+    if len(s) > 0:
+        s = re.sub('\s+', ' ', s)
+        if s[-1] == " ":
+            s = s[:-1]
+    return s
+
+
+def normalize_name(group):
+    group.name = delete_whitespace(group.name)
+    return group
+
+
 @pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
 def test_add_group(gen, group):
     old_group_list = gen.group.get_group_list()
     gen.group.create(group)
     assert len(old_group_list) + 1 == gen.group.get_group_count()
     new_group_list = gen.group.get_group_list()
-    old_group_list.append(group)
+    old_group_list.append(normalize_name(group))
+    print(old_group_list)
     assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
 
 
-def test_add_empty_group(gen):
+def test_add_group_with_whitespace(gen):
     old_group_list = gen.group.get_group_list()
-    group = Group(name="", header="", footer="")
+    group = Group(name="nameE7zwRa ", header="header_groupISZ", footer="footer1S tph")
     gen.group.create(group)
     assert len(old_group_list) + 1 == gen.group.get_group_count()
     new_group_list = gen.group.get_group_list()
-    old_group_list.append(group)
+    old_group_list.append(normalize_name(group))
     assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
 
 
