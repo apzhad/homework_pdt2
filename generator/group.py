f69ed865ad414b8e721db407463023d8cc9a4fbd
Anna <apzhadovets@gmail.com>
2019-03-16 00:28:49 +0300

    добавлены опции для генерации данных: -c - количество генерируемых групп (по умолчанию 5) -f - файл, в который будут записаны тестовые данные (по умолчанию data/group.json)

--- generator/group.py
+++ generator/group.py
@@ -3,6 +3,26 @@ import random
 import string
 import os.path
 import json
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "c:f:", ["count of groups", "file for data"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+
+c = 5
+f = "data/group.json"
+
+
+for o, a in opts:
+    if o == "-c":
+        c = int(a)
+    elif o == "-f":
+        f = a
 
 
 def random_string(prefix, max_length):
@@ -12,11 +32,11 @@ def random_string(prefix, max_length):
 
 test_data = [Group(name="", header="", footer="")] + [
     Group(name=random_string("name", 10), header=random_string("header_group", 20), footer=random_string("footer", 20))
-    for i in range(5)
+    for i in range(c)
 ]
 
 
-data_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/group.json")
+data_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(data_file, "w") as data:
     data.write(json.dumps(test_data, default=lambda x: x.__dict__, indent=2))
