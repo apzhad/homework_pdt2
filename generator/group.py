8eb9a3f5a7b27211cbf3e21be89551439446aa6b
Anna <apzhadovets@gmail.com>
2019-03-16 22:48:23 +0300

    вместо json используется jsonpickle

--- generator/group.py
+++ generator/group.py
@@ -2,7 +2,7 @@ from model.group import Group
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -39,4 +39,5 @@ test_data = [Group(name="", header="", footer="")] + [
 data_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(data_file, "w") as data:
-    data.write(json.dumps(test_data, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    data.write(jsonpickle.encode(test_data))
