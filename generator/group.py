ec7620995c04afbe66192bd6af7969e2b5c0da6c
Anna <apzhadovets@gmail.com>
2019-03-17 00:27:12 +0300

    изменены опции для создания тестового набора групп

--- generator/group.py
+++ generator/group.py
@@ -8,21 +8,21 @@ import sys
 
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "c:f:", ["count of groups", "file for data"])
+    opts, args = getopt.getopt(sys.argv[1:], "cg:fg:", ["count of groups", "file for group data"])
 except getopt.GetoptError as err:
     getopt.usage()
     sys.exit(2)
 
 
-c = 5
-f = "data/group.json"
+cg = 5
+fg = "data/group.json"
 
 
 for o, a in opts:
-    if o == "-c":
-        c = int(a)
-    elif o == "-f":
-        f = a
+    if o == "-cg":
+        cg = int(a)
+    elif o == "-fg":
+        fg = a
 
 
 def random_string(prefix, max_length):
@@ -32,11 +32,11 @@ def random_string(prefix, max_length):
 
 test_data = [Group(name="", header="", footer="")] + [
     Group(name=random_string("name", 10), header=random_string("header_group", 20), footer=random_string("footer", 20))
-    for i in range(c)
+    for i in range(cg)
 ]
 
 
-data_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+data_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", fg)
 
 with open(data_file, "w") as data:
     jsonpickle.set_encoder_options("json", indent=2)
