790d6656d75b1aa3df09af622a2d095c0b474d28
Anna <apzhadovets@gmail.com>
2019-03-23 00:57:44 +0300

    добавлено получение списка групп из БД

--- tests/test_del_group.py
+++ tests/test_del_group.py
@@ -1,60 +1,57 @@
 from model.group import Group
-from random import randrange
+import random
 
 
-def test_del_first_group(gen):
-    if gen.group.get_group_count() == 0:
+def test_del_first_group(gen, db):
+    if len(db.get_group_list()) == 0:
         gen.group.create(Group(name="for_del"))
     old_group_list = gen.group.get_group_list()
     gen.group.del_first()
-    assert len(old_group_list) - 1 == gen.group.get_group_count()
+    assert len(old_group_list) - 1 == len(db.get_group_list())
     new_group_list = gen.group.get_group_list()
     old_group_list[0:1] = []
     assert old_group_list == new_group_list
 
 
-def test_del_all_group(gen):
-    if gen.group.get_group_count() == 0:
+def test_del_all_group(gen, db):
+    if len(db.get_group_list()) == 0:
         gen.group.create(Group(name="for_del_1"))
         gen.group.create(Group(name="for_del_2"))
         gen.group.create(Group(name="for_del_3"))
     gen.group.del_all()
-    assert 0 == gen.group.get_group_count()
-    new_group_list = gen.group.get_group_list()
+    new_group_list = db.get_group_list()
     assert [] == new_group_list
 
 
-def test_del_last_group(gen):
-    if gen.group.get_group_count() == 0:
+def test_del_last_group(gen, db):
+    if len(db.get_group_list()) == 0:
         gen.group.create(Group(name="for_del_1"))
         gen.group.create(Group(name="for_del_2"))
         gen.group.create(Group(name="for_del_3"))
     old_group_list = gen.group.get_group_list()
     gen.group.del_last()
-    assert len(old_group_list) - 1 == gen.group.get_group_count()
+    assert len(old_group_list) - 1 == len(db.get_group_list())
     new_group_list = gen.group.get_group_list()
     old_group_list[-1:] = []
     assert old_group_list == new_group_list
 
 
-def test_del_without_choice_group(gen):
-    if gen.group.get_group_count() == 0:
+def test_del_without_choice_group(gen, db):
+    if len(db.get_group_list()) == 0:
         gen.group.create(Group(name="for_del_1"))
         gen.group.create(Group(name="for_del_2"))
-    old_group_list = gen.group.get_group_list()
+    old_group_list = db.get_group_list()
     gen.group.del_not_choose()
-    assert len(old_group_list) == gen.group.get_group_count()
-    new_group_list = gen.group.get_group_list()
+    new_group_list = db.get_group_list()
     assert old_group_list == new_group_list
 
 
-def test_del_some_group(gen):
-    if gen.group.get_group_count() == 0:
+def test_del_some_group(gen, db):
+    if len(db.get_group_list()) == 0:
         gen.group.create(Group(name="for_del"))
-    old_group_list = gen.group.get_group_list()
-    index = randrange(len(old_group_list))
-    gen.group.del_by_index(index)
-    assert len(old_group_list) - 1 == gen.group.get_group_count()
-    new_group_list = gen.group.get_group_list()
-    old_group_list[index:index+1] = []
+    old_group_list = db.get_group_list()
+    group = random.choice(old_group_list)
+    gen.group.del_by_id(group.id)
+    new_group_list = db.get_group_list()
+    old_group_list.remove(group)
     assert old_group_list == new_group_list
