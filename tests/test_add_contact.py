258a2a50c7b34677128be3bdd0dbd0a46087300c
Anna <apzhadovets@gmail.com>
2019-03-23 23:00:34 +0300

    для проверки список контактов загружается из БД; отключаемая проверка веба

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -7,33 +7,39 @@ month = ("January", "February", "March", "April", "May", "June", "July",
          "August", "September", "October", "November", "December")
 
 
-def test_add_contact_using_json(gen, json_contact):
+def test_add_contact_using_json(gen, db, json_contact, check_ui):
     contact = json_contact
-    old_contact_list = gen.contact.get_contact_list()
+    old_contact_list = db.get_contact_list()
     group = gen.group.get_groups_names() + ["[none]"]
     contact.group_name = random.choice(group)
     gen.contact.create(contact)
-    assert len(old_contact_list) + 1 == gen.contact.get_contact_count()
-    new_contact_list = gen.contact.get_contact_list()
+    assert len(old_contact_list) + 1 == len(db.get_contact_list())
+    new_contact_list = db.get_contact_list()
     old_contact_list.append(contact)
     assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contact_list, key=Contact.id_or_max) == sorted(gen.contact.get_contact_list(),
+                                                                         key=Contact.id_or_max)
 
 
-def test_add_contact_using_module(gen, data_contacts):
+def test_add_contact_using_module(gen, data_contacts, check_ui, db):
     contact = data_contacts
-    old_contact_list = gen.contact.get_contact_list()
+    old_contact_list = db.get_contact_list()
     group = gen.group.get_groups_names() + ["[none]"]
     contact.group_name = random.choice(group)
     gen.contact.create(contact)
-    assert len(old_contact_list) + 1 == gen.contact.get_contact_count()
-    new_contact_list = gen.contact.get_contact_list()
+    assert len(old_contact_list) + 1 == len(db.get_contact_list())
+    new_contact_list = db.get_contact_list()
     old_contact_list.append(contact)
     assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contact_list, key=Contact.id_or_max) == sorted(gen.contact.get_contact_list(),
+                                                                         key=Contact.id_or_max)
 
 
-def test_add_contact_into_group(gen):
+def test_add_contact_into_group(gen, db, check_ui):
     group = gen.group.get_groups_names() + ["[none]"]
-    old_contact_list = gen.contact.get_contact_list()
+    old_contact_list = db.get_contact_list()
     cont = Contact(first_name="first_name ", middle_name="middle_name", last_name="last_name", nickname="nickname",
                    title="title", company="company", address="address", home_phone="homephone",
                    mobile_phone="mobilephone", work_phone="workphone", fax="fax", primary_email="email",
@@ -43,16 +49,22 @@ def test_add_contact_into_group(gen):
                    secondary_home_phone="home secondary", notes="notes", photo_path="cat.jpg")
     cont.group_name = random.choice(group)
     gen.contact.create(cont)
-    assert len(old_contact_list) + 1 == gen.contact.get_contact_count()
-    new_contact_list = gen.contact.get_contact_list()
+    assert len(old_contact_list) + 1 == len(db.get_contact_list())
+    new_contact_list = db.get_contact_list()
     old_contact_list.append(cont)
     assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contact_list, key=Contact.id_or_max) == sorted(gen.contact.get_contact_list(),
+                                                                         key=Contact.id_or_max)
 
 
-def test_add_empty_contact(gen):
-    old_contact_list = gen.contact.get_contact_list()
+def test_add_empty_contact(gen, db, check_ui):
+    old_contact_list = db.get_contact_list()
     gen.contact.create_empty()
-    assert len(old_contact_list) + 1 == gen.contact.get_contact_count()
-    new_contact_list = gen.contact.get_contact_list()
+    assert len(old_contact_list) + 1 == len(db.get_contact_list())
+    new_contact_list = db.get_contact_list()
     old_contact_list.append(Contact(first_name="", last_name=""))
     assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contact_list, key=Contact.id_or_max) == sorted(gen.contact.get_contact_list(),
+                                                                         key=Contact.id_or_max)
