44c7da95698344b31411c3290070bd263b53c836
Anna <apzhadovets@gmail.com>
2019-03-23 13:23:40 +0300

    добавлена проверка списка конактов из вею и БД

--- tests/test_check_match_db_and_web.py
+++ tests/test_check_match_db_and_web.py
@@ -1,7 +1,14 @@
 from model.group import Group
+from model.contact import Contact
 
 
 def test_group_list(gen, db):
     web_group = gen.group.get_group_list()
     db_group = db.get_group_list()
     assert sorted(web_group, key=Group.id_or_max) == sorted(db_group, key=Group.id_or_max)
+
+
+def test_contact_list(gen, db):
+    web_contact = gen.contact.get_contact_list()
+    db_contact = db.get_contact_list()
+    assert sorted(web_contact, key=Contact.id_or_max) == sorted(db_contact, key=Contact.id_or_max)
