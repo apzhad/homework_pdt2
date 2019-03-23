6a03badf7efd3ac44f182cb1a8b8eb9cac09b98c
Anna <apzhadovets@gmail.com>
2019-03-23 20:02:50 +0300

    добавлен метод редактирования группы по id

--- fixture/group.py
+++ fixture/group.py
@@ -91,6 +91,18 @@ class GroupManage:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def edit_by_id(self, id, group):
+        wd = self.gen.wd
+        self.open_groups_page()
+        # select group & init editing
+        self.select_group_by_id(id)
+        wd.find_element_by_name("edit").click()
+        # change group parameters
+        self.enter_group_parameters(group)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
     def update_first_wo_change(self):
         self.update_wo_change_by_index(0)
 
@@ -99,6 +111,16 @@ class GroupManage:
         self.open_groups_page()
         # select group & init editing
         self.select_group_by_index(index)
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
+    def update_wo_change_by_id(self, id):
+        wd = self.gen.wd
+        self.open_groups_page()
+        # select group & init editing
+        self.select_group_by_id(id)
         wd.find_element_by_name("edit").click()
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
