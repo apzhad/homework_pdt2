249dd949b8c56af45df89756a573d05f9cc8c6df
Anna <apzhadovets@gmail.com>
2019-03-24 14:39:58 +0300

    убран conv=decoders, тк он приводил к ошибке TypeError: no default type converter defined

--- fixture/orm.py
+++ fixture/orm.py
@@ -28,7 +28,7 @@ class ORMFixture:
                      lazy=True)
 
     def __init__(self, host, name, user, password):
-        self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders)
+        self.db.bind('mysql', host=host, database=name, user=user, password=password)
         self.db.generate_mapping()
 
     def convert_gr_to_model(self, groups):
