0d7f1f25418b0cbbe78d23938ada7b0d8e779028
Anna <apzhadovets@gmail.com>
2019-03-24 11:52:24 +0300

    добавлена связь между контактами и группами

--- fixture/orm.py
+++ fixture/orm.py
@@ -15,6 +15,8 @@ class ORMFixture:
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups",
+                       lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
@@ -22,6 +24,8 @@ class ORMFixture:
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
         deprecated = Optional(datetime, column='deprecated')
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="contacts",
+                     lazy=True)
 
     def __init__(self, host, name, user, password):
         self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders)
@@ -44,4 +48,15 @@ class ORMFixture:
     @db_session
     def get_group_list(self):
         return self.convert_cont_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
+
+    @db_session
+    def get_contact_in_group(self, group):
+        orm_group = list(select(gr for gr in ORMFixture.ORMGroup if gr.id == group.id))[0]
+        return self.convert_cont_to_model(orm_group.contacts)
+
+    @db_session
+    def get_contact_not_in_group(self, group):
+        orm_group = list(select(gr for gr in ORMFixture.ORMGroup if gr.id == group.id))[0]
+        return self.convert_cont_to_model(
+            select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
 
