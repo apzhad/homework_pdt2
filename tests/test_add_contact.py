26bbf4a640f3d6006e8844172daa2fb096ffd046
Anna <apzhadovets@gmail.com>
2019-04-13 20:11:20 +0300

    добавлены шаги для генерации отчетов allure

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -2,6 +2,7 @@
 from model.contact import Contact
 from model.group import Group
 import random
+import allure
 
 
 month = ("January", "February", "March", "April", "May", "June", "July",
@@ -10,65 +11,81 @@ month = ("January", "February", "March", "April", "May", "June", "July",
 
 def test_add_contact_using_json(gen, db, json_contact, check_ui):
     contact = json_contact
-    old_contact_list = db.get_contact_list()
-    group = db.get_group_list()
-    group.append(Group(id="[none]", name="[none]"))
-    contact.group_name = random.choice(group).id
-    gen.contact.create(contact)
-    assert len(old_contact_list) + 1 == len(db.get_contact_list())
-    new_contact_list = db.get_contact_list()
-    old_contact_list.append(contact)
-    assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
-    if check_ui:
-        assert sorted(new_contact_list, key=Contact.id_or_max) == sorted(gen.contact.get_contact_list(),
-                                                                         key=Contact.id_or_max)
+    with allure.step('Given a contact list'):
+        old_contact_list = db.get_contact_list()
+    with allure.step('Given a group list and choose a group from it'):
+        group = db.get_group_list()
+        group.append(Group(id="[none]", name="[none]"))
+        contact.group_name = random.choice(group).id
+    with allure.step('When I add the contact to the list'):
+        gen.contact.create(contact)
+    with allure.step('Then the new contact list is equal to the old list with the added contac'):
+        assert len(old_contact_list) + 1 == len(db.get_contact_list())
+        new_contact_list = db.get_contact_list()
+        old_contact_list.append(contact)
+        assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
+        if check_ui:
+            assert sorted(new_contact_list, key=Contact.id_or_max) == sorted(gen.contact.get_contact_list(),
+                                                                             key=Contact.id_or_max)
 
 
 def test_add_contact_using_module(gen, data_contacts, check_ui, db):
     contact = data_contacts
-    old_contact_list = db.get_contact_list()
-    group = db.get_group_list()
-    group.append(Group(id="[none]", name="[none]"))
-    contact.group_name = random.choice(group).id
-    gen.contact.create(contact)
-    assert len(old_contact_list) + 1 == len(db.get_contact_list())
-    new_contact_list = db.get_contact_list()
-    old_contact_list.append(contact)
-    assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
-    if check_ui:
-        assert sorted(new_contact_list, key=Contact.id_or_max) == sorted(gen.contact.get_contact_list(),
-                                                                         key=Contact.id_or_max)
+    with allure.step('Given a contact list'):
+        old_contact_list = db.get_contact_list()
+    with allure.step('Given a group list and choose a group from it'):
+        group = db.get_group_list()
+        group.append(Group(id="[none]", name="[none]"))
+        contact.group_name = random.choice(group).id
+    with allure.step('When I add the contact to the list'):
+        gen.contact.create(contact)
+    with allure.step('Then the new contact list is equal to the old list with the added contac'):
+        assert len(old_contact_list) + 1 == len(db.get_contact_list())
+        new_contact_list = db.get_contact_list()
+        old_contact_list.append(contact)
+        assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
+        if check_ui:
+            assert sorted(new_contact_list, key=Contact.id_or_max) == sorted(gen.contact.get_contact_list(),
+                                                                             key=Contact.id_or_max)
 
 
 def test_add_contact_into_group(gen, db, check_ui):
-    group = db.get_group_list()
-    group.append(Group(id="[none]", name="[none]"))
-    old_contact_list = db.get_contact_list()
-    cont = Contact(first_name="first_name ", middle_name="middle_name", last_name="last_name", nickname="nickname",
-                   title="title", company="company", address="address", home_phone="homephone",
-                   mobile_phone="mobilephone", work_phone="workphone", fax="fax", primary_email="email",
-                   secondary_email="email2", third_email="email3", homepage="homepage", birth_day="1",
-                   birth_month=random.choice(month), birth_year="1950", anniversary_day="15", anniversary_month="June",
-                   anniversary_year="2000", secondary_address="address secondary",
-                   secondary_home_phone="home secondary", notes="notes", photo_path="cat.jpg")
-    cont.group_name = random.choice(group).id
-    gen.contact.create(cont)
-    assert len(old_contact_list) + 1 == len(db.get_contact_list())
-    new_contact_list = db.get_contact_list()
-    old_contact_list.append(cont)
-    assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
-    if check_ui:
-        assert sorted(new_contact_list, key=Contact.id_or_max) == sorted(gen.contact.get_contact_list(),
+    with allure.step('Given a contact list'):
+        old_contact_list = db.get_contact_list()
+    with allure.step('Given a contact to add'):
+        cont = Contact(first_name="first_name ", middle_name="middle_name", last_name="last_name", nickname="nickname",
+                       title="title", company="company", address="address", home_phone="homephone",
+                       mobile_phone="mobilephone", work_phone="workphone", fax="fax", primary_email="email",
+                       secondary_email="email2", third_email="email3", homepage="homepage", birth_day="1",
+                       birth_month=random.choice(month), birth_year="1950", anniversary_day="15", anniversary_month="June",
+                       anniversary_year="2000", secondary_address="address secondary",
+                       secondary_home_phone="home secondary", notes="notes", photo_path="cat.jpg")
+    with allure.step('Given a group list and choose a group from it'):
+        group = db.get_group_list()
+        group.append(Group(id="[none]", name="[none]"))
+        cont.group_name = random.choice(group).id
+    with allure.step('When I add the contact to the list'):
+        gen.contact.create(cont)
+    with allure.step('Then the new contact list is equal to the old list with the added contac'):
+        assert len(old_contact_list) + 1 == len(db.get_contact_list())
+        new_contact_list = db.get_contact_list()
+        old_contact_list.append(cont)
+        assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
+        if check_ui:
+            assert sorted(new_contact_list, key=Contact.id_or_max) == sorted(gen.contact.get_contact_list(),
                                                                          key=Contact.id_or_max)
 
 
 def test_add_empty_contact(gen, db, check_ui):
-    old_contact_list = db.get_contact_list()
-    gen.contact.create_empty()
-    assert len(old_contact_list) + 1 == len(db.get_contact_list())
-    new_contact_list = db.get_contact_list()
-    old_contact_list.append(Contact(first_name="", last_name=""))
-    assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
-    if check_ui:
-        assert sorted(new_contact_list, key=Contact.id_or_max) == sorted(gen.contact.get_contact_list(),
-                                                                         key=Contact.id_or_max)
+    with allure.step('Given a contact list'):
+        old_contact_list = db.get_contact_list()
+    with allure.step('When I add empty contact to the list'):
+        gen.contact.create_empty()
+    with allure.step('Then the new contact list is equal to the old list with the added contac'):
+        assert len(old_contact_list) + 1 == len(db.get_contact_list())
+        new_contact_list = db.get_contact_list()
+        old_contact_list.append(Contact(first_name="", last_name=""))
+        assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
+        if check_ui:
+            assert sorted(new_contact_list, key=Contact.id_or_max) == sorted(gen.contact.get_contact_list(),
+                                                                             key=Contact.id_or_max)
