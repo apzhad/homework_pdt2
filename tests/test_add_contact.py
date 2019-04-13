c81c380f3fb8815744f0de1aa73a4d1b94e79a5d
Anna <apzhadovets@gmail.com>
2019-04-13 22:01:42 +0300

    в шаги добавлена информация о добавляемом контакте

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -16,10 +16,11 @@ def test_add_contact_using_json(gen, db, json_contact, check_ui):
     with allure.step('Given a group list and choose a group from it'):
         group = db.get_group_list()
         group.append(Group(id="[none]", name="[none]"))
-        contact.group_name = random.choice(group).id
-    with allure.step('When I add the contact to the list'):
+        group = random.choice(group)
+        contact.group_name = group.id
+    with allure.step('When I add the contact %s to the list and into the group "%s"' % (contact, group.name)):
         gen.contact.create(contact)
-    with allure.step('Then the new contact list is equal to the old list with the added contac'):
+    with allure.step('Then the new contact list is equal to the old list with the added contact'):
         assert len(old_contact_list) + 1 == len(db.get_contact_list())
         new_contact_list = db.get_contact_list()
         old_contact_list.append(contact)
@@ -36,10 +37,11 @@ def test_add_contact_using_module(gen, data_contacts, check_ui, db):
     with allure.step('Given a group list and choose a group from it'):
         group = db.get_group_list()
         group.append(Group(id="[none]", name="[none]"))
-        contact.group_name = random.choice(group).id
-    with allure.step('When I add the contact to the list'):
+        group = random.choice(group)
+        contact.group_name = group.id
+    with allure.step('When I add the contact %s to the list and into the group "%s"' % (contact, group.name)):
         gen.contact.create(contact)
-    with allure.step('Then the new contact list is equal to the old list with the added contac'):
+    with allure.step('Then the new contact list is equal to the old list with the added contact'):
         assert len(old_contact_list) + 1 == len(db.get_contact_list())
         new_contact_list = db.get_contact_list()
         old_contact_list.append(contact)
@@ -57,16 +59,17 @@ def test_add_contact_into_group(gen, db, check_ui):
                        title="title", company="company", address="address", home_phone="homephone",
                        mobile_phone="mobilephone", work_phone="workphone", fax="fax", primary_email="email",
                        secondary_email="email2", third_email="email3", homepage="homepage", birth_day="1",
-                       birth_month=random.choice(month), birth_year="1950", anniversary_day="15", anniversary_month="June",
-                       anniversary_year="2000", secondary_address="address secondary",
+                       birth_month=random.choice(month), birth_year="1950", anniversary_day="15",
+                       anniversary_month="June", anniversary_year="2000", secondary_address="address secondary",
                        secondary_home_phone="home secondary", notes="notes", photo_path="cat.jpg")
     with allure.step('Given a group list and choose a group from it'):
         group = db.get_group_list()
         group.append(Group(id="[none]", name="[none]"))
-        cont.group_name = random.choice(group).id
-    with allure.step('When I add the contact to the list'):
+        group = random.choice(group)
+        cont.group_name = group.id
+    with allure.step('When I add the contact %s to the list and into the group "%s"' % (cont, group.name)):
         gen.contact.create(cont)
-    with allure.step('Then the new contact list is equal to the old list with the added contac'):
+    with allure.step('Then the new contact list is equal to the old list with the added contact'):
         assert len(old_contact_list) + 1 == len(db.get_contact_list())
         new_contact_list = db.get_contact_list()
         old_contact_list.append(cont)
@@ -81,7 +84,7 @@ def test_add_empty_contact(gen, db, check_ui):
         old_contact_list = db.get_contact_list()
     with allure.step('When I add empty contact to the list'):
         gen.contact.create_empty()
-    with allure.step('Then the new contact list is equal to the old list with the added contac'):
+    with allure.step('Then the new contact list is equal to the old list with the added contact'):
         assert len(old_contact_list) + 1 == len(db.get_contact_list())
         new_contact_list = db.get_contact_list()
         old_contact_list.append(Contact(first_name="", last_name=""))
