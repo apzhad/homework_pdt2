f7fc76096e1ab56afe62d37a02d9110180c273fe
Anna <apzhadovets@gmail.com>
2019-04-14 14:33:58 +0300

    добавлены проверки и тест на удаление группы

--- rf/Addressbook.py
+++ rf/Addressbook.py
@@ -1,3 +1,8 @@
+"""
+Библиотека для Robot Framework 3.0
+Запуск тестов осуществляется через python -m robot rf
+"""
+
 import pytest
 from fixture.generic import Generic
 from fixture.db import DbFixture
@@ -31,5 +36,18 @@ class Addressbook:
         self.fixture.finish()
         self.dbfixture.finish()
 
-    def create_group(self, name, header, footer):
-        self.fixture.group.create(Group(name=name, header=header, footer=footer))
+    def get_group_list(self):
+        return self.dbfixture.get_group_list()
+
+    def new_group(self, name, header, footer):
+        return Group(name=name, header=header, footer=footer)
+
+    def create_group(self, group):
+        self.fixture.group.create(group)
+
+    def delete_group(self, group):
+        self.fixture.group.del_by_id(group.id)
+
+    def group_lists_should_be_equal(self, list1, list2):
+        assert sorted(list1, key=Group.id_or_max) == sorted(list2, key=Group.id_or_max)
+
